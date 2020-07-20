b30fb12e5354a55b915ab990d6e0f03970ef38eb
Jun Ito <jun_ito@voyagegroup.com>
2020-07-20 20:21:19 +0900

    Update: trait

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,26 @@
-use std::num::ParseIntError;
-// type Result<T> = std::result::Result<T, ParseIntError>;
+fn main() {}
 
-fn half_number(s: &str) -> Result<i32, ParseIntError> {
-    let res = s.parse::<i32>();
-    match res {
-        Ok(n) => Ok(n / 2),
-        Err(e) => Err(e),
+struct Rectangle {
+    width: f32,
+    heigit: f32,
+}
+
+struct Circle {
+    radius: f32,
+}
+
+trait CalcArea {
+    fn calc_area(&self) -> f32;
+}
+
+impl CalcArea for Rectangle {
+    fn calc_area(&self) -> f32 {
+        self.width * self.heigit
     }
 }
 
-fn half_number2(s: &str) -> Result<i32, ParseIntError> {
-    s.parse::<i32>().map(|n| n / 2)
-}
-
-fn half_number3(s: &str) -> Result<i32, ParseIntError> {
-    let res = s.parse::<i32>()?;
-    return Ok(res / 2);
-}
-
-fn half_number4(s: &str) -> Result<i32, &str> {
-    let res = s.parse::<i32>();
-    match res {
-        Ok(n) => Ok(n / 2),
-        Err(_) => Err("パースに失敗しました"),
+impl CalcArea for Circle {
+    fn calc_area(&self) -> f32 {
+        self.radius * self.radius * 3.14
     }
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let n = "100x";
-    let res = half_number3(n)?;
-    println!("{}", res);
-
-    Ok(())
 }
