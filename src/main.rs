ef6f7631f86df23c22e4d73d74eda0851b0a8bd8
Jun Ito <jun_ito@voyagegroup.com>
2020-07-20 20:15:53 +0900

    Update: error_check

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,35 @@
-fn main() {
-    let mut pa = Person::new("Jun", 29, "Shinsen");
+use std::num::ParseIntError;
+// type Result<T> = std::result::Result<T, ParseIntError>;
 
-    pa.print();
-    pa.print_t(true);
-    pa.print_t(false);
-    println!("{}", pa.to_str());
-    pa.add_age(1);
-    println!("{}", pa.to_str());
-
-    let maaya = Person::new("maaya", 27, "Sancha");
-    println!("{}", maaya.to_str());
+fn half_number(s: &str) -> Result<i32, ParseIntError> {
+    let res = s.parse::<i32>();
+    match res {
+        Ok(n) => Ok(n / 2),
+        Err(e) => Err(e),
+    }
 }
 
-struct Person {
-    id: i32,
-    name: String,
-    age: i32,
-    addr: String,
+fn half_number2(s: &str) -> Result<i32, ParseIntError> {
+    s.parse::<i32>().map(|n| n / 2)
 }
-static mut PERSON_ID: i32 = 0;
 
-impl Person {
-    fn new(name: &str, age: i32, addr: &str) -> Person {
-        let new_id = unsafe {
-            PERSON_ID += 1;
-            PERSON_ID
-        };
+fn half_number3(s: &str) -> Result<i32, ParseIntError> {
+    let res = s.parse::<i32>()?;
+    return Ok(res / 2);
+}
 
-        Person {
-            id: new_id,
-            name: name.to_string(),
-            age: age,
-            addr: addr.to_string(),
-        }
+fn half_number4(s: &str) -> Result<i32, &str> {
+    let res = s.parse::<i32>();
+    match res {
+        Ok(n) => Ok(n / 2),
+        Err(_) => Err("パースに失敗しました"),
     }
-    fn print(&self) {
-        println!("{}, {}, {}, {}", self.id, self.name, self.age, self.addr);
-    }
+}
 
-    fn print_t(&self, private: bool) {
-        if private {
-            println!("{}, {}", self.id, self.name);
-        } else {
-            println!("{}, {}, {}, {}", self.id, self.name, self.age, self.addr);
-        }
-    }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let n = "100x";
+    let res = half_number3(n)?;
+    println!("{}", res);
 
-    fn to_str(&self) -> String {
-        format!("{}, {}({}), {}", self.id, self.name, self.age, self.addr)
-    }
-
-    fn add_age(&mut self, n: i32) {
-        self.age += n;
-    }
+    Ok(())
 }
