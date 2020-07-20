b2af020ad7ed19262b0d00fcf485d64f8b47325f
Jun Ito <jun_ito@voyagegroup.com>
2020-07-20 23:55:26 +0900

    Update: network access

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-fn main() {
-    let a = String::from("1");
-    let b = &a;
-    // a = String::from("2");
-    println!("{}", a);
-    println!("{}", b);
+use reqwest::StatusCode;
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "http://abehiroshi.la.coocan.jp/hoi";
+    dbg!(url);
+    let res = reqwest::get(url).await?;
+    match res.status() {
+        StatusCode::OK => {
+            let body = res.text().await?;
+            println!("response is \n{}", body);
+        }
+        StatusCode::NOT_FOUND => {
+            dbg!("not found");
+        }
+        _ => {
+            dbg!("other errors");
+        }
+    }
+    Ok(())
 }
