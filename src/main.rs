32bbecb898f8762b2fa4cb015687bd3e2021f8a7
Jun Ito <jun_ito@voyagegroup.com>
2020-07-20 23:10:24 +0900

    Add: random lib

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-fn main() {}
-
-struct Rectangle {
-    width: f32,
-    heigit: f32,
-}
-
-struct Circle {
-    radius: f32,
-}
-
-trait CalcArea {
-    fn calc_area(&self) -> f32;
-}
-
-impl CalcArea for Rectangle {
-    fn calc_area(&self) -> f32 {
-        self.width * self.heigit
-    }
-}
-
-impl CalcArea for Circle {
-    fn calc_area(&self) -> f32 {
-        self.radius * self.radius * 3.14
-    }
+fn main() {
+    let a = String::from("1");
+    let b = &a;
+    // a = String::from("2");
+    println!("{}", a);
+    println!("{}", b);
 }
