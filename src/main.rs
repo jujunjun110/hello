eb1eb07bf9f544c5fe90881095fbacfd689cbff2
Jun Ito <jun_ito@voyagegroup.com>
2020-07-21 22:21:59 +0900

    Update: cli base db tool

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,41 @@ struct User {
 }
 
 fn main() -> Result<()> {
-    let cn = Connection::open_in_memory()?;
-    cn.execute(
-        "CREATE TABLE users (id INTEGER, name TEXT, age INTEGER)",
-        params![],
-    )?;
+    let args = std::env::args().collect::<Vec<String>>();
+    let cn = Connection::open("sample.db")?;
 
-    let mut stmt = cn.prepare("INSERT INTO users (id, name, age) VALUES (?, ?, ?)")?;
-    stmt.execute(params![1, "Kongo", 20])?;
-    stmt.execute(params![2, "Hieai", 20])?;
-    dbg!("WORKING!");
-
-    let mut stmt = cn.prepare("SELECT * FROM users")?;
-
-    let iter = stmt.query_map(params![], |row| {
-        Ok(User {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            age: row.get(2)?,
-        })
-    })?;
-
-    for it in iter {
-        println!("{:?}", it.unwrap());
+    if args.len() <= 1 {
+        let mut stmt = cn.prepare("SELECT * FROM users")?;
+        for it in stmt.query_map(params![], |row| {
+            Ok(User {
+                id: row.get(0)?,
+                name: row.get(1)?,
+                age: row.get(2)?,
+            })
+        })? {
+            println!("{:?}", it.unwrap());
+        }
+    } else {
+        match args[1].as_str() {
+            "init" => {
+                println!("create database");
+                cn.execute(
+                    "CREATE TABLE users (id INTEGER, name TEXT, age INTEGER)",
+                    params![],
+                )?;
+            }
+            "into" => {
+                let id: i32 = args[2].parse::<i32>().unwrap();
+                let name = &args[3];
+                let age: i32 = args[4].parse::<i32>().unwrap();
+                let mut stmt = cn.prepare("INSERT INTO users (id, name, age) VALUES (?, ?, ?)")?;
+                stmt.execute(params![id, name, age])?;
+                println!("insert data!");
+            }
+            _ => {
+                println!("parameter error!");
+            }
+        }
     }
 
     Ok(())
